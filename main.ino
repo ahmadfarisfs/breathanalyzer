4627506f6838e76f04ae47260c0a6ed38ced6dd2
ahmadfarisfs <ahmadfarisfs@gmail.com>
2020-08-10 00:19:31 +0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,6 +5,7 @@ void setup() {
     pinMode(heaterSelPin,OUTPUT);   // set the heaterSelPin as digital output.
     digitalWrite(heaterSelPin,LOW); // Start to heat the sensor
 }
+
  float ethanolInCO(uint16_t co)
 {
     float etanol;
@@ -31,6 +32,7 @@ void setup() {
     }
     return etanol;
 }
+
 float ppm2mgL(uint16_t ppm)
 {
     float mgL;
@@ -38,23 +40,19 @@ float ppm2mgL(uint16_t ppm)
     mgL = (1.82 * ppm) / 1000.0;
     return mgL;
 }
+
 void loop() {
     float sensor_volt;
     float RS_air; //  Get the value of RS via in a clear air
     float sensorValue = 0;
- 
-/*--- Get a average data by testing 100 times ---*/
-    for(int x = 0 ; x < 10 ; x++)
+     for(int x = 0 ; x < 10 ; x++)
     {
         sensorValue = sensorValue + analogRead(A5);
     }
     sensorValue = sensorValue/10.0;
-/*-----------------------------------------------*/
     uint16_t CO_ppm = map(sensorValue,0,1024,1,1000);
     float ethanol_ppm = ethanolInCO(CO_ppm);
     float ethanol_mgl = ppm2mgL(ethanol_ppm);
-//    sensor_volt = (sensorValue/1024)*5.0;
-    //RS_air = sensor_volt/(5.0-sensor_volt); // omit *R16
     Serial.println(ethanol_mgl);
     delay(100);
 }
